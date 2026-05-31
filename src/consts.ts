a16b600d008cfc2051cb7da8235520ef97f53e79
Mikko Pallas <mikko.pallas@lapinamk.fi>
2026-05-31 23:01:23 +0300

    Migrate to Astro with minimalist design and add GH Pages workflow

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Mikko Pallas';
+export const SITE_DESCRIPTION = 'Data Engineer & Knowledge Management';
